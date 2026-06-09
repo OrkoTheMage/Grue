09d54e9849c052665299a1823b4c55e3a389d172
OrkoTheMage <aeryngrindle@gmail.com>
2026-06-09 13:47:14 -0500

    version update

--- src/components/CLI.js
+++ src/components/CLI.js
@@ -244,7 +244,7 @@ export default function CLI() {
         case "v":
         case "--v":
         case "--version":
-          displayMsg("Grue.sh v1.1.1")
+          displayMsg("Grue.sh v1.1.2")
           break
 
         case "ps":
